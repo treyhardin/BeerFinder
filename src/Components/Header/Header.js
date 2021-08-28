0fab6505a43e3d6e5bac74cab954ce3bef224866
Trey Hardin <treyhardindesign@gmail.com>
2021-08-28 16:52:57 -0400

    Active link styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 export default function Headers() {
     return (
         <header>
             <Link to='/' className='logo u-h2'><p>BrewFinder</p></Link>
-            <Link to='/' className='navLink u-h2'><p>Search</p></Link>
-            <Link to='/explore' className='navLink u-h2'><p>Explore</p></Link>
+            <NavLink exact={true} activeClassName='active' to='/' className='navLink u-h2'><p>Search</p></NavLink>
+            <NavLink exact={true} to='/explore' className='navLink u-h2'><p>Explore</p></NavLink>
         </header>
     )
 }
