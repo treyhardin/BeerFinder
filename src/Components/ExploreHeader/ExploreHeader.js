d12f4e89127577bf7a2b64dbfe3ca4b95e9a0cdf
Trey Hardin <treyhardindesign@gmail.com>
2021-08-29 14:21:28 -0400

    Remove Filter

--- src/Components/ExploreHeader/ExploreHeader.js
+++ src/Components/ExploreHeader/ExploreHeader.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './ExploreHeader.css';
-import Filter from '../Filter/Filter';
 
 export default function ExploreHeader() {
     return (
         <div className='exploreHeader'>
             <h1>Explore</h1>
-            {/* <Filter /> */}
         </div>
     )
 }
